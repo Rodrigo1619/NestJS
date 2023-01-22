b5ac8240b6b99ce8021dee92b9f6e6acb6a11a6d
rodrigo1619 <00067920@uca.edu.sv>
2023-01-22 17:03:05 -0600

    getters, setters

--- IntroduccionTS/src/bases/03-classes.ts
+++ IntroduccionTS/src/bases/03-classes.ts
@@ -9,14 +9,33 @@
     }
 } */
 //Otra forma mas simplicaifcada de hacerlo es
-    export class Pokemon{
+export class Pokemon{
+
+    //getters
+    get imageUrl():string{
+        return `https://pokemon.com/${this.id}.jpg`;
+    }
 
     constructor(
     public readonly id:number, 
-    public name:string
+    public name:string,
     ){}
+
+    //Metodos: no son mas que funciones, esto lo vimos en POO con C# y lo podemos ver como las acciones que hace nuestro pokemon
+    scream(){
+        console.log(`${this.name.toUpperCase()}!!!`);
+    }
+    speak(){
+        console.log(`${this.name}, ${this.name}`);
+    }
 }
 //El readonly es para que no se pueda modificar el parametro de id
 export const charmander = new Pokemon(4, 'Charmander');
+console.log(charmander)
+
+//utilizando metodos
+charmander.speak();
+charmander.scream();
+
 /* charmander.id = 10;
 charmander.name = 'Miu ' */
