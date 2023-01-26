140c1dd492bf2ef5ff93ef029ed607979109750e
rodrigo1619 <00067920@uca.edu.sv>
2023-01-26 16:52:13 -0600

    feat: getting a car by id (basic way)

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+    private cars = ['Toyota', 'Honda', 'Jeep'];
+
     //con el decorador Get le decimos que cuando venga una peticion get al endpoint cars ejecute la funcion de obtener los carros
     @Get()
     getAllCars(){
-        return ['Toyota', 'Honda', 'Jeep'];
+        return this.cars
+    }
+
+    @Get(':id')
+    getCarById( @Param('id')id:string){
+        console.log({id})
+        return {
+            car: this.cars[id]
+        }
     }
 }
